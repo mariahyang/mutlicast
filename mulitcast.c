861a162eed714364992b7816af9087d0864cab3e
Mariah Yang <galm@163.com>
2016-01-30 00:04:25 +0800

    update

--- mulitcast.c
+++ mulitcast.c
@@ -1,6 +1,4 @@
-/* 
-
-multicast.c
+t.c
 
 The following program sends or receives multicast packets. If invoked
 with one argument, it sends a packet containing the current time to an
@@ -9,9 +7,7 @@ arguments, it receives and prints these packets. Start it as a sender on
 just one host and as a receiver on all the other hosts
 
 */
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -22,11 +18,10 @@ just one host and as a receiver on all the other hosts
 #define EXAMPLE_PORT 6000
 #define EXAMPLE_GROUP "224.222.222.200"
 
-int main(int argc, char **argv)
+main(int argc)
 {
    struct sockaddr_in addr;
-   int sock, cnt;
-   socklen_t addrlen;
+   int addrlen, sock, cnt;
    struct ip_mreq mreq;
    char message[50];
 
@@ -46,41 +41,41 @@ int main(int argc, char **argv)
       /* send */
       addr.sin_addr.s_addr = inet_addr(EXAMPLE_GROUP);
       while (1) {
-   time_t t = time(0);
-   sprintf(message, "time is %-24.24s", ctime(&t));
-   printf("sending: %s\n", message);
-   cnt = sendto(sock, message, sizeof(message), 0,
-          (struct sockaddr *) &addr, addrlen);
-   if (cnt < 0) {
-      perror("sendto");
-      exit(1);
-   }
-   sleep(5);
+	 time_t t = time(0);
+	 sprintf(message, "time is %-24.24s", ctime(&t));
+	 printf("sending: %s\n", message);
+	 cnt = sendto(sock, message, sizeof(message), 0,
+		      (struct sockaddr *) &addr, addrlen);
+	 if (cnt < 0) {
+ 	    perror("sendto");
+	    exit(1);
+	 }
+	 sleep(5);
       }
    } else {
 
       /* receive */
       if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {        
          perror("bind");
-   exit(1);
+	 exit(1);
       }    
       mreq.imr_multiaddr.s_addr = inet_addr(EXAMPLE_GROUP);         
       mreq.imr_interface.s_addr = htonl(INADDR_ANY);         
       if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
-         &mreq, sizeof(mreq)) < 0) {
-   perror("setsockopt mreq");
-   exit(1);
+		     &mreq, sizeof(mreq)) < 0) {
+	 perror("setsockopt mreq");
+	 exit(1);
       }         
       while (1) {
-   cnt = recvfrom(sock, message, sizeof(message), 0, 
-      (struct sockaddr *) &addr, &addrlen);
-   if (cnt < 0) {
-      perror("recvfrom");
-      exit(1);
-   } else if (cnt == 0) {
-      break;
-   }
-   printf("%s: message = \"%s\"\n", inet_ntoa(addr.sin_addr), message);
+ 	 cnt = recvfrom(sock, message, sizeof(message), 0, 
+			(struct sockaddr *) &addr, &addrlen);
+	 if (cnt < 0) {
+	    perror("recvfrom");
+	    exit(1);
+	 } else if (cnt == 0) {
+ 	    break;
+	 }
+	 printf("%s: message = \"%s\"\n", inet_ntoa(addr.sin_addr), message);
         }
     }
 }
